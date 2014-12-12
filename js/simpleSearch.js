5fad702fb87025648823eb378e1b273efcd9eb46
Seung Lee <shl7@uw.edu>
2014-12-11 18:42:21 -0800

    Cleaned up image fetching and added ingredients
    what title says

--- js/simpleSearch.js
+++ js/simpleSearch.js
@@ -78,7 +78,7 @@ function renderResults(docs, $container, $template){
     $container.empty(); // If there are any previous results, remove them
     $( "#front-page-content" ).remove();
     var result;
-    $.each(docs, function(index, doc){
+    $.each(docs, function(index, doc) {
         //result = $template.clone();
         //result.find( "a" ).prop( "href", doc.url);
         //result.find( "h3" ).append( doc.title );
@@ -86,37 +86,11 @@ function renderResults(docs, $container, $template){
         //result.find( ".content" ).append( maxWords(doc.content, 100) );
         // result.find(".content").append(pictureResults(doc.url, doc.content));
         //result.find(".col-lg-2 > img").attr("src", based(doc.url));
-        // getIngredients(doc.url, doc.content);
         //result.removeClass( "template" );
         //$container.append(result);
-        var image = "";
-        if(/^.*\b(epicurious.com\/recipes)\b.*$/.test(doc.url)){
-            $.getJSON('http://whateverorigin.org/get?url=' + encodeURIComponent(doc.url) + '&callback=?', function(data){
-                var elements = $(data.contents);
-                var found = $('#recipe_image > img', elements);
-                //var image = "<img src=\"http://www.epicurious.com/" + found.attr("src") + "\" alt=\"image\">";
-                image = "http://www.epicurious.com" + found.attr("src");
-            })
-            .success(function() { var result = $('<a>', {href: doc.url, class:"results"});
-        result.append("<div class=\"search-result\"><div class=\"row\"><div class=\"col-lg-2\"><img src=\""+ image +"\" alt=\"image\" width=\"125px\" height=\"125px\"></div><div class=\"col-lg-10\"><h3 class=\"recipe-title\">" + doc.title + "</h3><div class=\"row\"> <div class=\"col-lg-6\"> <ul><li>Makes 4 servings</li> <li>Preperation Time: 15 minutes</li>   <li>Cooking Time: 1 hour</li></ul> </div><div class=\"col-lg-6\"> <ul><li>1/2 tsp garlic powder</li><li>4 chicken breasts</li><li>1 whole tomato</li> <li>1/4 tbs tears of children</li> </ul></div> </div><!-- end row ingredients --></div> <!-- end ingredients column --></div> <!-- end row image/ingredients --> </div> <!-- end search-result --></a>");
-        //result.find("img").attr("src", based(doc.url));
-        $('#results').append(result);
-         });
-        }
+        getResults(doc);
 
-        if(/^.*\b(allrecipes)\b.*$/.test(doc.url)){
-            $.getJSON('http://whateverorigin.org/get?url=' + encodeURIComponent(doc.url) + '&callback=?', function(data){
-                var elements = $(data.contents);
-                var found = $('#imgPhoto', elements);
-                image = found.attr("src");
-            })
-            .success(function() { var result = $('<a>', {href: doc.url, class:"results"});
-        result.append("<div class=\"search-result\"><div class=\"row\"><div class=\"col-lg-2\"><img src=\""+ image +"\" alt=\"image\"></div><div class=\"col-lg-10\"><h3 class=\"recipe-title\">" + doc.title + "</h3><div class=\"row\"> <div class=\"col-lg-6\"> <ul><li>Makes 4 servings</li> <li>Preperation Time: 15 minutes</li>   <li>Cooking Time: 1 hour</li></ul> </div><div class=\"col-lg-6\"> <ul><li>1/2 tsp garlic powder</li><li>4 chicken breasts</li><li>1 whole tomato</li> <li>1/4 tbs tears of children</li> </ul></div> </div><!-- end row ingredients --></div> <!-- end ingredients column --></div> <!-- end row image/ingredients --> </div> <!-- end search-result --></a>");
-        $('#results').append(result);
-         });
-        }
     });
-    //document.getElementById("results").style.display = "block";
 }
 
 // Cuts off lengthy content to a given maximum number of words
@@ -176,24 +150,84 @@ function noSuggestions($container) {
     $container.append(result);
 }
 
-/*
-function getIngredients(url, content) {
+
+function getIngredients(url) {
     var allRecipe = "http://allrecipes.com/Recipe";
     var epicurious = "http://www.epicurious.com/recipes/food";
 
+    var ingredients = new Array();
+/*
    if (url.startsWith(allRecipe)) {
         
     }
-        
+*/        
     if (url.startsWith(epicurious)) {
+        $.getJSON('http://whateverorigin.org/get?url=' + encodeURIComponent(url) + '&callback=?', function(data) {
+            var elements = $(data.contents);
+            var found = $('.ingredient', elements);
+            
+            for (var i = 0; i < found.length; i++) {
+                ingredients[i] = found[i].innerText;
+                // alert(ingredients[i]);
+            }
+        })
+        .success(function() { 
 
+        });
+        
     }       
- 
-    // allrecipe
-    // div class="ingred-left"
-    //
-    // epicurious
-    // http://www.epicurious.com/recipes/food
-    // ul class="ingredientsList first no-header" -> ingredients
-    // ul class="ingredientsList no-header" -> special equipment
-*/
+}
+
+
+function getResults(doc) {
+    var image = "";
+    var ingredients = new Array();
+
+    if(/^.*\b(epicurious.com\/recipes)\b.*$/.test(doc.url)) {
+        $.getJSON('http://whateverorigin.org/get?url=' + encodeURIComponent(doc.url) + '&callback=?', function(data){
+            var elements = $(data.contents);
+            var imgFound = $('#recipe_image > img', elements);
+            image = "http://www.epicurious.com" + imgFound.attr("src");
+            
+            var ingFound = $('.ingredient', elements);
+            for (var i = 0; i < ingFound.length; i++) {
+                ingredients[i] = ingFound[i].innerText;
+            }
+        })
+        .success(function() { 
+            attachImage(image, doc.title, doc.url);
+            attachIngredients(ingredients);
+        });
+    } else if(/^.*\b(allrecipes)\b.*$/.test(doc.url)) {
+        $.getJSON('http://whateverorigin.org/get?url=' + encodeURIComponent(doc.url) + '&callback=?', function(data) {
+            var elements = $(data.contents);
+            var found = $('#imgPhoto', elements);
+            image = found.attr("src");
+        })
+        .success(function() { 
+            attachImage(image, doc.title, doc.url);
+            attachIngredients(ingredients);
+        });
+    }
+}
+
+function attachImage(image, title, url) {
+    var result = $('<a>', { 
+                href: url, 
+                class:"results"
+    });
+
+    result.append("<div class=\"search-result\"><div class=\"row\"><div class=\"col-lg-2\"><img src=\""+ image +"\" alt=\"image\" width=\"125px\" height=\"125px\"></div><div class=\"col-lg-10\"><h3 class=\"recipe-title\">" + title + "</h3><div class=\"row\"> <div class=\"col-lg-6\"> <ul><li>Makes 4 servings</li> <li>Preperation Time: 15 minutes</li>   <li>Cooking Time: 1 hour</li></ul> </div><div class=\"col-lg-6\"> <ul><li>1/2 tsp garlic powder</li><li>4 chicken breasts</li><li>1 whole tomato</li> <li>1/4 tbs tears of children</li> </ul></div> </div><!-- end row ingredients --></div> <!-- end ingredients column --></div> <!-- end row image/ingredients --> </div> <!-- end search-result --></a>");
+    $('#results').append(result);
+}
+
+function attachIngredients(ingredients) {
+    var result = document.createElement("ul");
+    result.className = "ingredients";
+    for (var i = 0; i < ingredients.length; i++) {
+        var li = document.createElement("li");
+        li.innerText = ingredients[i];
+        result.appendChild(li);
+    }
+    $('#results').append(result);
+}
